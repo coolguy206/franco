4d9b34342a2b73e398bad938e35ebbbc77c6b228
David Thanphilom <davidthanphilom@msn.com>
2016-05-21 01:01:17 -0700

    added backbone routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,50 +1,47 @@
 
-module.exports = [
-	{
-		method: 'GET',
-		path: '/',
-		handler: require('../handlers/home')
-	},
+var Home = require('../views/homeView');
+var Post = require('../views/postView');
+var Sidebar = require('../views/sidebarView');
+var aboutContent = require('../models/about');
+var contactContent = require('../models/contact');
 
-	{
-		method: 'GET',
-		path: '/post/{num?}',
-		handler: require('../handlers/posts')
+module.exports = Backbone.Router.extend({
+	
+	routes: {
+		'': 'home',
+		'about': 'about',
+		'contact': 'contact',
+		'post/:num': 'post'
 	},
+	home: function(){
+		// console.log('this is the home page');
+		var home = new Home();
+		home.render();
+	},
+	about: function(){
+		// console.log('this is the about page');
+		var about = new Sidebar({
+			// el: '.main-content',
+			tagName:'div', 
+			id: 'about'
+		});
+		about.render(aboutContent);
+		$('.main-content').html('<div class="bg"></div>');
+		$('.bg').html(about.$el);
 
-	{
-		method: 'GET',
-		path:'/about',
-		handler: require('../handlers/about')
 	},
-
-	{
-		method: 'GET',
-		path:'/contact',
-		handler: require('../handlers/contact')
+	contact: function(){
+		// console.log('this is the contact page');
+		var contact = new Sidebar({
+			tagName: 'div',
+			id: 'contact'
+		});
+		contact.render(contactContent);
+		$('.main-content').html('<div class="bg"></div>');
+		$('.bg').html(contact.$el);
 	},
-
-	{
-		method: 'GET',
-		path: '/success',
-		handler: require('../handlers/success')
-	},
-
-	{
-		method: 'POST',
-		path:'/contact',
-		handler: require('../handlers/postReq')
-	},
-
-	{
-		method: 'POST',
-		path:'/',
-		handler: require('../handlers/postReq')
-	},
-
-	{
-		method: 'POST',
-		path: '/post/{num?}',
-		handler: require('../handlers/postReq')
-	},
-];
+	post: function(num){
+		// console.log('this is the ' + num + ' post');
+		var post = new Post(num);
+	}
+});
