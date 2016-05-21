4a08dfe4e37122aeef53e078da42af873039a29d
David Thanphilom <davidthanphilom@msn.com>
2016-05-21 00:56:38 -0700

    module exported the models.js

--- models/models.js
+++ models/models.js
@@ -1,31 +1,17 @@
 
-var Backbone = require('backbone');
-var najax = require('najax');
-Backbone.ajax = najax;
-
-
-var Page = Backbone.Model.extend({
-	url:'https://api.instagram.com/v1/users/1344367478/media/recent?access_token=3187374881.526fc9f.451aa6c0691d4c128ae4d46bc5b54b17',	
-});
-
-
-var page = new Page();
-var theArray = [];
-var data = page.fetch({
-	success: function(obj){
-		// console.log(user.toJSON().data[0].link);
-		var instagram = obj.toJSON().data;
-		instagram.forEach(function(e,i){
-			theArray.push(e);
-
-		});
-		// console.log(instagram);
+module.exports = Backbone.Model.extend({
+	url:'https://api.instagram.com/v1/users/263705258/media/recent?access_token=3187374881.526fc9f.451aa6c0691d4c128ae4d46bc5b54b17',
+	defaults: {
+		data: ''
 	},
-	error: function(err){
-		console.log('no');
-	}
+	getContent: function(callback){
+		this.fetch({
+			dataType: 'jsonp',
+			success: function(obj){
+				// console.log('all done from server');
+				callback(obj.toJSON());
+			}
+		});
+	}	
 });
-
-// console.log(page.toJSON());
-module.exports = theArray;
 
