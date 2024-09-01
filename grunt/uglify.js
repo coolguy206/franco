d89f37c3e83381a4b09cfe5c97915c715f94bfd8
David Thanphilom <davidthanphilom@msn.com>
2024-08-31 22:42:14 -0700

    still coding 2024.static.site

--- grunt/uglify.js
+++ grunt/uglify.js
@@ -3,7 +3,7 @@ module.exports = {
 
   options: {
     compress: true,
-    sourceMap: true
+    // sourceMap: true
   },
 
   default: {
--- js/babel/scripts.js
+++ js/babel/scripts.js
@@ -1,2 +1,17 @@
+var $ = require('jquery');
+$(document).ready(function () {
+  // console.log('ready');
 
+  $('.the-video').click(function () {
+    var src = $(this).attr('data-video');
+    // console.log(src);
+
+    var elem = '<div class="overlay"></div><div class="video-player"><div class="close-icon"><img src="imgs/global/rectangle-xmark-solid.svg" alt="close icon"></div><video autoplay controls><source src="' + src + '" type="video/mp4">Your browser does not support the video tag.</video><img src="imgs/global/filipino-star-bay-area.svg" alt="filipino star bay area"></div>';
+    $('main').append(elem);
+    $('.close-icon').on('click', function () {
+      $('.overlay, .video-player').remove();
+      // console.log('close clicked');
+    });
+  });
+});
 //# sourceMappingURL=scripts.js.map
