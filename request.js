5f0d79c44bc240866ed75b1837a9e4b80fcf94a1
David Thanphilom <davidthanphilom@msn.com>
2024-09-18 18:45:05 -0700

    updated hp css 2024.static.site

--- request.js
+++ request.js
@@ -1,13 +1,21 @@
 const request = require('request');
 const fs = require('fs');
+//? 1
+// var url = `https://www.sfexaminer.com/sports/warriors/warriors-hype-man-franco-finn-carries-nba-role-into-day-job/article_04b1f18e-0705-11ee-9c61-3bc15b6df7d4.html`;
 
 // var name = `Warriors-biggest-hype-man-has-a-day-job-Hype-up-everyone-else`;
 
-// var url = `https://www.sfexaminer.com/sports/warriors/warriors-hype-man-franco-finn-carries-nba-role-into-day-job/article_04b1f18e-0705-11ee-9c61-3bc15b6df7d4.html`;
+//? 2
+// var url = `https://www.nytimes.com/athletic/4218492/2023/02/17/nba-all-star-warriors-hype-man-franco-finn/`;
 
-var name = `Warriors-hype-man-earns-NBA-All-Star-honor-Heres-why-his-booming-voice-matters`;
+// var name = `nba-all-star-warriors-hype-man-franco-finn`;
 
-var url = `https://www.nytimes.com/athletic/4218492/2023/02/17/nba-all-star-warriors-hype-man-franco-finn/`;
+//? 3
+var url = `https://www.cbsnews.com/sanfrancisco/news/franco-finn-doesnt-have-a-jump-shot-but-hes-an-nba-all-star-this-weekend/`;
+
+var name = `franco-finn-doesnt-have-a-jump-shot-but-hes-an-nba-all-star-this-weekend`;
+
+
 
 request(url, function (error, response, body) {
     // console.error('error:', error); // Print the error if one occurred
