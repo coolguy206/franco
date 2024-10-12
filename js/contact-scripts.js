d43ff3f0f36cecc7feb1fb01b620a3bd723a42ec
David Thanphilom <davidthanphilom@msn.com>
2024-10-11 18:39:13 -0700

    need to css mobile contact page and figure out how to send email still

---
+++ js/contact-scripts.js
@@ -0,0 +1,36 @@
+var $ = require('jquery');
+var overlayVideo = require('./overlay-video.js');
+var phoneConvert = require('./phone.js');
+require('dotenv').config();
+
+$(document).ready(function () {
+
+    // console.log(overlayVideo.overlayVideo);
+    overlayVideo.overlayVideo('../')
+
+    // console.log(process.env);
+    //<input type="hidden" name="access_key" value="768508d4-cb2a-46ca-86a9-7b517093344b">
+
+    var options = [
+        `auctioneer`,
+        `wedding`,
+        `dj`,
+        `host`,
+        `other`,
+    ]
+
+    $(options).each(function (i, val) {
+        // console.log(i, val);
+        var elem = ` <option value="${val}">${val}</option>`;
+        $(`select`).append(elem);
+    });
+
+    function emailIsValid(email) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+    }
+
+    $('form input[name="phone"]').on('input', function (e) {
+        $(this).val($(this).val().replace(/[^0-9]/g, ''));
+    });
+
+});
